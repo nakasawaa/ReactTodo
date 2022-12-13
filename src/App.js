0cc0124d5eeaacad168af8c25c3a3f2cdb858846
nakasawaa <c.cancino@ecole-ipssi.net>
2022-12-13 13:34:59 +0100

    TodolistReact

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import LocalStorage from "./components/LocalStorage";
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
